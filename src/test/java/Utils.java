f5e325d0c0d444d632b7f9795d84b9d6b22dd631
Jasmine <jasabd-0@student.ltu.se>
2022-05-04 22:06:25 +0200

    Initial commit

---
+++ src/test/java/Utils.java
@@ -0,0 +1,5 @@
+public class Utils {
+    final static String BASE_URL = "http://demo.guru99.com/test/login.html";
+    final static String CHROME_DRIVER_LOCATION = "C:\\Users\\jasmi\\OneDrive\\Skrivbord\\chromedriver\\chromedriver.exe";
+}
+
